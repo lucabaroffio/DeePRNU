3035ca18626d9c06851102da6c40f9ae1b9c3d7e
lucabaroffio <luca.baroffio@polimi.it>
2015-11-05 08:11:08 +0100

    create DB completed

--- CreateDB/create_Dresden_siamese.py
+++ CreateDB/create_Dresden_siamese.py
@@ -7,8 +7,9 @@ import sys
 
 
 WINSIZE = 52 # window size
-NUMPAIRS = 100 # number of training pairs
+NUMPAIRS = 100000 # number of training pairs
 POSRATIO = 0.5 # ratio of positive samples, P/(P + N)
+TOUGHRATIO = 0.5 # ratio of tough non-matching pairs, i.e. patches from the same image
 DATAPATH = '/data/dataset/Dresden/natural'
 CAMERAFILE = 'Dresden_cameraList'
 
@@ -32,6 +33,7 @@ def getMatchingImagesPath(filename, filelist):
 	for c in cameras:
 		if (filename.find(c)>0):
 			curCam = c
+			break
 	# print filename	
 	# print curCam
 	assert(curCam!=None)
@@ -39,28 +41,50 @@ def getMatchingImagesPath(filename, filelist):
 	# matching = [x for x in xrange(0, len(filelist)) if ((curCam in filelist[x]) & (filelist[x]!=filename))]
 	return matching
 
+def getCamera(filename):
+	
+	curCam = None
+	with open(CAMERAFILE) as f:
+		cameras = f.read().splitlines()
+	for c in cameras:
+		if (filename.find(c)>0):
+			curCam = c
+			break
+	# print filename	
+	# print curCam
+	assert(curCam!=None)
+	return curCam
+
 
 def main():
 
 	random.seed(666)
-	assert (not os.path.exists('matching'))
-	assert (not os.path.exists('non-matching'))
+	assert (not os.path.exists('patches'))
 
-	os.makedirs('matching')
-	os.makedirs('matching/1')
-	os.makedirs('matching/2')
+	os.makedirs('patches')
+	os.makedirs('patches/1')
+	os.makedirs('patches/2')
 
 	filelist = glob.glob(os.path.join(DATAPATH, '*.JPG'))
 
 	numPos = int(POSRATIO*NUMPAIRS)
 	numNeg = int(NUMPAIRS - numPos)
+	numTough = int(numNeg*TOUGHRATIO)
+
+	f1 = open('patches/1/fileList.txt', 'w')
+	f2 = open('patches/2/fileList.txt', 'w') 
 
 	# positive pairs
 	print 'creating positive pairs...'
 	for x in xrange(numPos):
 		drawProgressBar(float(x)/numPos)
-		im1Path = random.choice(filelist) # open a random image
-		im1 = cv2.imread(im1Path)
+		while(1):
+			im1Path = random.choice(filelist) # open a random image
+			im1 = cv2.imread(im1Path)
+			if ((im1.shape[0]>WINSIZE) and (im1.shape[1]>WINSIZE)):
+				break
+			print 'invalid image: ' + im1Path
+
 		nRows = im1.shape[0]
 		nCols = im1.shape[1]
 		nWindRows = int((nRows - WINSIZE)/WINSIZE) # get the number of hor and vert windows
@@ -71,16 +95,79 @@ def main():
 		
 
 		# get a matching window
-		im2Path = random.choice(getMatchingImagesPath(im1Path, filelist))
-		im2 = cv2.imread(im2Path)
+		while(1):
+			im2Path = random.choice(getMatchingImagesPath(im1Path, filelist))
+			im2 = cv2.imread(im2Path)
+			if ((im1.shape[0]>WINSIZE) and (im1.shape[1]>WINSIZE)):
+				break
+			print 'invalid image: ' + im2Path
+
 		wind2 = im2[curWind[0]*WINSIZE:(curWind[0]*WINSIZE + WINSIZE), curWind[1]*WINSIZE:(curWind[1]*WINSIZE + WINSIZE), :]
-		assert((wind2.shape[0] == WINSIZE) & (wind2.shape[1] == WINSIZE))
+		if not((wind2.shape[0] == WINSIZE) & (wind2.shape[1] == WINSIZE)):
+			continue
 
-		cv2.imwrite(os.path.join('matching', '1', ("%08d" % x) + '.jpg'), wind1, [cv.CV_IMWRITE_JPEG_QUALITY, 100])
+		cv2.imwrite(os.path.join('patches', '1', ("%08d" % (2*x)) + '.jpg'), wind1, [cv.CV_IMWRITE_JPEG_QUALITY, 100])
 
-		cv2.imwrite(os.path.join('matching', '2', ("%08d" % x) + '.jpg'), wind2, [cv.CV_IMWRITE_JPEG_QUALITY, 100])
+		cv2.imwrite(os.path.join('patches', '2', ("%08d" % (2*x)) + '.jpg'), wind2, [cv.CV_IMWRITE_JPEG_QUALITY, 100])
+
+		f1.write(os.path.join('patches', '1', ("%08d" % (2*x)) + '.jpg') + ' 1\n')
+		f2.write(os.path.join('patches', '2', ("%08d" % (2*x)) + '.jpg') + ' 1\n')
 	
 	drawProgressBar(1.0)
+
+	print 'creating negative pairs...'
+	for x in xrange(numNeg):
+		drawProgressBar(float(x)/numNeg)
+		while(1):
+			im1Path = random.choice(filelist) # open a random image
+			im1 = cv2.imread(im1Path)
+			if ((im1.shape[0]>WINSIZE) and (im1.shape[1]>WINSIZE)):
+				break
+			print 'invalid image: ' + im1Path
+
+		nRows = im1.shape[0]
+		nCols = im1.shape[1]
+		nWindRows = int((nRows - WINSIZE)/WINSIZE) # get the number of hor and vert windows
+		nWindCols = int((nCols - WINSIZE)/WINSIZE) 
+		curWind = [random.choice(xrange(nWindRows)), random.choice(xrange(nWindCols))] # random window
+		wind1 = im1[curWind[0]*WINSIZE:(curWind[0]*WINSIZE + WINSIZE), curWind[1]*WINSIZE:(curWind[1]*WINSIZE + WINSIZE), :]
+		assert((wind1.shape[0] == WINSIZE) & (wind1.shape[1] == WINSIZE))
+		
+
+		# get a non-matching window
+		if (x < numTough):
+			im2 = im1
+		else:
+			while(1):
+				im2Path = random.choice(filelist)
+				if (getCamera(im2Path) != getCamera(im1Path)):
+					im2 = cv2.imread(im2Path)
+					if ((im2.shape[0]>WINSIZE) and (im2.shape[1]>WINSIZE)):
+						break
+					print 'invalid image: ' + im2Path
+
+		nRows = im2.shape[0]
+		nCols = im2.shape[1]
+		nWindRows = int((nRows - WINSIZE)/WINSIZE) # get the number of hor and vert windows
+		nWindCols = int((nCols - WINSIZE)/WINSIZE) 
+		while(1):
+			curWind2 = [random.choice(xrange(nWindRows)), random.choice(xrange(nWindCols))] # random window
+			if ((curWind2[0] != curWind[0]) and (curWind2[0] != curWind[0])):
+				break
+
+		wind2 = im2[curWind2[0]*WINSIZE:(curWind2[0]*WINSIZE + WINSIZE), curWind2[1]*WINSIZE:(curWind2[1]*WINSIZE + WINSIZE), :]
+		assert((wind2.shape[0] == WINSIZE) & (wind2.shape[1] == WINSIZE))
+
+		cv2.imwrite(os.path.join('patches', '1', ("%08d" % (2*x + 1)) + '.jpg'), wind1, [cv.CV_IMWRITE_JPEG_QUALITY, 100])
+
+		cv2.imwrite(os.path.join('patches', '2', ("%08d" % (2*x + 1)) + '.jpg'), wind2, [cv.CV_IMWRITE_JPEG_QUALITY, 100])
+
+		f1.write(os.path.join('patches', '1', ("%08d" % (2*x + 1)) + '.jpg') + ' 0\n')
+		f2.write(os.path.join('patches', '2', ("%08d" % (2*x + 1)) + '.jpg') + ' 0\n')
+	
+	drawProgressBar(1.0)
+	f1.close()
+	f2.close()
 
 		
 		
